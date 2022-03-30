98e455436193cf285b17e5fbd26c3671a280167a
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-30 11:45:55 +0300

    create add note

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React,{ Component } from "react";
+import AddItems from "./components/AddItems/AddItems";
 
 class  App extends Component{
+  state = {
+    items: [
+      { id: 1, note: "Deena" },
+      { id: 2, note: "sereen" },
+      { id: 3, note: "text" },
+    ],
+  };
+  addItem = (item) => {
+    let { items } = this.state;
+    item.id = this.state.items[items.length - 1].id + 1;
+    items.push(item);
+    console.log(items);
+    this.setState(items);
+  };
+
   render() {
     return (
       <div className="App">
-        <p>todo app</p>
+          <p>todo app</p>
+        <AddItems addItem ={this.addItem} />
       </div>
     );
   }
-  
 }
 
 export default App;
